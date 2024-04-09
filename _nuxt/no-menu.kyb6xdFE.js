c26939fec85b6c8c8d663824fb10a501e8faf5e8
ZonD80 <zond80@me.com>
2024-04-09 13:33:39 +0300

    deployment

---
+++ _nuxt/no-menu.kyb6xdFE.js
@@ -0,0 +1,2 @@
+import{a as c,_ as n,b as _}from"./v-footer.vue.D-DDRq7t.js";import{_ as l,c as r,b as e,w as f,o as m,a as i,C as d}from"./entry.MyYndEul.js";import"./nuxt-link.CY5ducQa.js";const u={},p={class:"flex h-full flex-col"},x={class:"flex-grow"};function w(o,h){const s=n,t=c,a=_;return m(),r("div",p,[e(s,{class:"w-full"}),e(t,{class:"flex w-full flex-grow gap-8"},{default:f(()=>[i("main",x,[d(o.$slots,"default")])]),_:3}),e(a,{class:"w-full"})])}const N=l(u,[["render",w]]);export{N as default};
+//# sourceMappingURL=no-menu.kyb6xdFE.js.map
